291d2b1f6060980afb77bc303c25887b79595097
Senath Sethmika <senathsethmika@gmail.com>
2025-09-02 11:51:48 +0000

    DONE BUT SAME ERROR

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, initializeAuth, browserLocalPersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -20,11 +20,7 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const storage = getStorage(app);
-
-// Initialize Firebase Authentication with local persistence
-const auth = initializeAuth(app, {
-  persistence: browserLocalPersistence
-});
+const auth = getAuth(app);
 
 
 export { app, db, auth, storage };
